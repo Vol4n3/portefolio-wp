14456aec4515767c17d8f8c65ebfd3f382bad2de
Vol4n3 <jcvolan@gmail.com>
2017-08-18 22:46:22 +0200

    retouch css header

--- js/main.js
+++ js/main.js
@@ -6,29 +6,16 @@ jQuery(document).ready(function () {
     const $ = jQuery;
     /*NAV EFFECT-CUT*/
     const $nav_effect_cut = $('.nav-effect-cut > li');
-    $nav_effect_cut.hover(function () {
-        setTimeout(function () {
-            $(this).addClass('drop')
-        }.bind(this), 300)
-    }, function () {
-        setTimeout(function () {
-            $(this).removeClass('drop')
-        }.bind(this), 300)
-    });
     $nav_effect_cut.each(function () {
         let text = $(this).children().text();
         $('<div class="top"><a>' + text + '</a></div>').appendTo(this);
         $('<div class="bottom"><a>' + text + '</a></div>').appendTo(this);
     });
-    function resizeHeaderBar(scrollTop) {
-        if (scrollTop > 133) {
-            $('.big-title').toggleClass('active');
-        }
-    }
-    class BigTitle{
-        constructor(scrollTop){
-           this.setActive(scrollTop);
-           this.element = $('.big-title');
+
+    class HeaderTitle{
+        constructor(element){
+           this.setActive($(document).scrollTop());
+           this.element = element;
         }
         setActive(scrollTop){
             this.active = scrollTop > 133 && !this.active
@@ -47,23 +34,19 @@ jQuery(document).ready(function () {
             }
         }
     }
-    const bigTitle = new BigTitle($(this).scrollTop());
+    const ht = new HeaderTitle( $('.big-title'));
     /*scroll event*/
     $(document).scroll(() => {
         let scrollTop = $(this).scrollTop();
-
         /*Header bar*/
-        bigTitle.resize(scrollTop);
-
+        ht.resize(scrollTop);
         /*Header img effect*/
-        $('.bottom-header').css('background-position', '50% ' + (-scrollTop - 100) + 'px');
+        $('.bottom-header').css('background-position', '50%' + (-scrollTop - 100) + 'px');
         $('.move-down').css('padding-top', scrollTop + 'px');
-
     });
     function fixSize() {
         $('.fix-top-header').height($('.top-header').height())
     }
-
     fixSize();
     /*Resize event*/
     window.addEventListener('resize', () => {
